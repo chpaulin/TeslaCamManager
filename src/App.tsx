b8dcf41fe40f84e9094688e356b51e8cde64bdce
Christian Paulin (ext) <paulich@nordax.nordaxfinans.com>
2020-01-09 08:08:13 +0100

    Testing react-video component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Player } from 'video-react';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+        width="10"
+        height="10"
+        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      />
     </div>
   );
 }
